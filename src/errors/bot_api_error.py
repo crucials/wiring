da1492bf557ce2dc088a8abb182691436bd5d63e
crucials <ensueness@gmail.com>
2024-06-22 20:42:08 +0500

    enhanced documentation of bot api error

--- src/errors/bot_api_error.py
+++ src/errors/bot_api_error.py
@@ -5,13 +5,20 @@ from multi_platform_resources import Platform
 
 class BotApiError(Exception):
     """
-    common class that represents an error occurred on some platform
+    common class that represents an http error occurred on some platform
     api interaction
 
-    ...
+    Attributes:
+        status_code: http error status code
     """
     def __init__(self, platform: Platform, explanation: Optional[str] = None,
                  status_code: Optional[int] = None):
+        """creates common http error that occurred on some platform api interaction
+
+        Args:
+            status_code: http error status code
+        """
+
         error_text = f'failed to perform some action through {platform} api'
 
         if explanation is not None:
