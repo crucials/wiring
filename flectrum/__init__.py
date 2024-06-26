c8a3e65eacb39254736d59320426fb9619c3a259
crucials <ensueness@gmail.com>
2024-06-26 07:35:20 +0500

    added more imports to package `__init__.py` file for convenience

--- flectrum/__init__.py
+++ flectrum/__init__.py
@@ -1,3 +1,6 @@
-from flectrum.multi_platform_resources import *
-from flectrum.multi_platform_bot import MultiPlatformBot, MultiPlatformValue
-from flectrum.bot_base import *
+from flectrum.multi_platform_bot import MultiPlatformBot, PlatformBotNotFoundError
+from flectrum.multi_platform_resources import (PlatformSpecificValue, MultiPlatformChat,
+                                               MultiPlatformChatGroup, MultiPlatformId,
+                                               MultiPlatformMessage, MultiPlatformUser,
+                                               MultiPlatformValue, Platform)
+from flectrum.bot_base import Bot, Command, CommandHandler
